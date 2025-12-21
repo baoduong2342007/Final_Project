4b54c5df98ea60f81320d1de015479527bb6a301
LePhanDucMan <lpdman2532@apcs.fitus.edu.vn>
2025-12-21 19:46:21 +0700

    Update Global.h

--- tmp/Global.h
+++ tmp/Global.h
@@ -1,10 +1,21 @@
 #pragma once
-#include <bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+#include <string>
 #include "Dynamic_array.h"
 #include "Transaction.h"
 #include "RecurringTransaction.h"
 #include "Trie.h"
 #include "Date.h"
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define WHITE   "\033[37m"
+#define GREY    "\033[90m"
 
 using namespace std;
 
