b9b37ead98ad79b5ce1e7fb01e4e404290337bde
LePhanDucMan <lpdman2532@apcs.fitus.edu.vn>
2025-12-28 10:45:35 +0700

    Update RecurringTransaction.h

--- PersonalFinanceManager/include/RecurringTransaction.h
+++ PersonalFinanceManager/include/RecurringTransaction.h
@@ -6,7 +6,8 @@
 using namespace std;
 
 struct RecurringTransaction {
-    Date start_date;
+    Date start_date; // internal 
+    Date display_start; // UI date
     Date end_date; // end_date = 0 0 0 => no limit
     int source;  // 1 = income, 2 = expense
     Trie_node* source_id;
@@ -15,13 +16,13 @@ struct RecurringTransaction {
     string description;
     // init
     RecurringTransaction(){
-        start_date = end_date = Date();
+        display_start = start_date = end_date = Date();
         source_id = wallet_id = nullptr;
         amount = 0;
     }
 
     bool operator == (const RecurringTransaction &other) const{
-        if (same_date(start_date , other.start_date) == false) return false;
+        if (same_date(display_start, other.display_start) == false) return false;
         if (same_date(end_date , other.end_date) == false) return false;
         if (source != other.source) return false;
         if (source_id != other.source_id) return false;
